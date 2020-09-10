daeb356c91a1d75b48e03e55df083c2f48e0f1b1
SsTtOoNnEe <1602965550@qq.com>
2020-09-10 23:57:48 +0800

    jianhan commits

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,10 +1,16 @@
 package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("stu")
 public class UserController {
 
-    static int num = 0;
+    @GetMapping("/list")
+    public String test(){
+        return "index";
+    }
 
 }
